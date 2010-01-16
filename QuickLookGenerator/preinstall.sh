86736764af498b99d483e93abdc09d921fbd148f
John Mifsud <imabuddha@gmail.com>
2010-01-16 01:01:14 -0500

    add pre-install shell scripts to remove any existing versions, this should help with re-indexing & ql update

---
+++ QuickLookGenerator/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# SGF-Tools Quicklook Generator preinstall.sh
+
+echo removing existing qlgenerator
+sudo rm -rf '/Library/Quicklook/SGF.qlgenerator'
---
+++ SpotlightImporter/preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# SGF-Tools Spotlight Importer preinstall.sh
+
+echo removing existing importer
+sudo rm -rf '/Library/Spotlight/SGF.mdimporter'
